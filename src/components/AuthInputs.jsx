8051df5d3b2a1ba44c6479a1217b54825ed85e6a
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2024-04-09 23:28:37 +0530

    Refactored the code to use generic names for validation status parameters

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -16,16 +16,16 @@ const Label = styled.label`
   font-weight: 700;
   letter-spacing: 0.1em;
   text-transform: uppercase;
-  color: ${(props) => props.emailNotValid ? '#f87171' : '#6b7280'};
+  color: ${(props) => props.invalid ? '#f87171' : '#6b7280'};
 `
 
 const Input = styled.input`
   width: 100%;
   padding: 0.75rem 1rem;
   line-height: 1.5;
-  background-color: ${(props) => props.emailNotValid ? '#fed2d2' : '#d1d5db'};
-  color: ${({emailNotValid}) => emailNotValid ? '#f87171' : '#374151'};
-  border: 1px solid ${(props) => props.emailNotValid ? '#f73f3f' : 'transparent'};
+  background-color: ${(props) => props.invalid ? '#fed2d2' : '#d1d5db'};
+  color: ${({invalid}) => invalid ? '#f87171' : '#374151'};
+  border: 1px solid ${(props) => props.invalid ? '#f73f3f' : 'transparent'};
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `
@@ -54,10 +54,10 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <ControlContainer>
         <p>
-          <Label emailNotValid = {emailNotValid}>Email</Label>
+          <Label invalid = {emailNotValid}>Email</Label>
           <Input
             type="email"
-            emailNotValid = {emailNotValid}
+            invalid = {emailNotValid}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
