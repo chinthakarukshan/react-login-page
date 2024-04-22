9d13deaf160f9fd40988d1f38a7dd2a39585db05
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2024-04-23 00:00:32 +0530

    Created a reusable combined component

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import {styled} from 'styled-components';
 
 import Button from './Button';
+import CombinedInput from './CombinedInput';
 
 
 const ControlContainer = styled.div`
@@ -9,27 +10,6 @@ const ControlContainer = styled.div`
   flex-direction: column;
   gap: 0.5rem;
   margin-bottom: 1.5rem;
-`
-
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${(props) => props.$invalid ? '#f87171' : '#6b7280'};
-`
-
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: ${(props) => props.$invalid ? '#fed2d2' : '#d1d5db'};
-  color: ${({$invalid}) => $invalid ? '#f87171' : '#374151'};
-  border: 1px solid ${(props) => props.$invalid ? '#f73f3f' : 'transparent'};
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `
 
 export default function AuthInputs() {
@@ -55,24 +35,20 @@ export default function AuthInputs() {
   return (
     <div id="auth-inputs">
       <ControlContainer>
-        <p>
-          <Label $invalid = {emailNotValid}>Email</Label>
-          <Input
+        
+          <CombinedInput labelName="Email"
+            invalid1 = {emailNotValid}
             type="email"
-            $invalid = {emailNotValid}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
-        </p>
-        <p>
-          <Label $invalid={passwordNotValid}>Password</Label>
-          <Input
+          <CombinedInput labelName="Password"
+            invalid1 = {passwordNotValid}
             type="password"
-            $invalid = {passwordNotValid}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
           />
-        </p>
+        
       </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
